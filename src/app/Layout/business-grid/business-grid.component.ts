962b27a1088e2c565808d7be57848be569f5299e
Cristian <crodg1023@outlook.com>
2024-08-10 09:00:08 +0200

    Cambio de indexService por el uso de signals

--- src/app/Layout/business-grid/business-grid.component.ts
+++ src/app/Layout/business-grid/business-grid.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, signal, Signal } from '@angular/core';
 import { BusinessCardComponent } from '../../Components/Utils/business-card/business-card.component';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { FiltersModalComponent } from '../../Components/Modals/filters-modal/filters-modal.component';
 import { ModalService } from '../../Services/modal.service';
 import { FiltersService } from '../../Services/Filters/filters.service';
 import { Filters } from '../../Interfaces/filters';
-import { IndexService } from '../../Services/Index/index.service';
 
 @Component({
   selector: 'app-business-grid',
@@ -19,15 +18,13 @@ export class BusinessGridComponent {
   displayed: any[] = [];
   category: string = '';
   count: number = 12;
-  currentIndex!: number;
+  currentIndex = signal(0);
   showing: number = 0;
   hasFiltered: boolean = false;
 
-  constructor(private route: ActivatedRoute, private router: Router, private modalService: ModalService, private filtersService: FiltersService, private indexService: IndexService) {}
+  constructor(private route: ActivatedRoute, private router: Router, private modalService: ModalService, private filtersService: FiltersService) {}
 
   ngOnInit(): void {
-    this.currentIndex = this.indexService.getCurrentIndex();
-
     this.route.data.subscribe(({ business }) => {
       this.business = business;
     });
@@ -50,22 +47,22 @@ export class BusinessGridComponent {
   }
 
   showMore() {
-    const nextIndex = this.currentIndex + this.count;
+    const nextIndex = this.currentIndex() + this.count;
     nextIndex >= this.business.length ? this.showing = this.business.length : this.showing = nextIndex;
     if (this.category) {
       this.displayed = this.business.filter(x => x.category === this.category);
     } else {
-      this.displayed = this.displayed.concat(this.business.slice(this.currentIndex, nextIndex));
+      this.displayed = this.displayed.concat(this.business.slice(this.currentIndex(), nextIndex));
     }
-    this.currentIndex = nextIndex;
-    this.indexService.setCurrentIndex(this.currentIndex);
+
+    this.currentIndex.set(nextIndex);
   }
 
   showLess() {
-    this.currentIndex >= this.count ? this.currentIndex -= this.count : this.currentIndex = 0;
-    this.showing = this.currentIndex;
-    this.displayed = this.displayed.slice(0, this.currentIndex);
-    this.indexService.setCurrentIndex(this.currentIndex);
+    const previousIndex = this.currentIndex() >= this.count ? this.currentIndex() - this.count : 0;
+    this.showing = previousIndex;
+    this.displayed = this.displayed.slice(0, previousIndex);
+    this.currentIndex.set(previousIndex);
   }
 
   filterByCategory() {
@@ -78,16 +75,14 @@ export class BusinessGridComponent {
   showAll() {
     this.displayed = this.business.slice(0, this.count);
     this.showing = this.displayed.length;
-    this.currentIndex = this.count;
+    this.currentIndex.set(this.count);
     this.hasFiltered = false;
-    this.indexService.setCurrentIndex(this.currentIndex);
     this.router.navigate(['/business']);
   }
 
   filter(filters: Filters) {
       this.displayed = this.business.filter(business => {
         const matchesName = !filters.name || this.removeAccent(business.name).toLocaleLowerCase().includes(this.removeAccent(filters.name).toLocaleLowerCase());
-
         const matchesCoutnry = !filters.country || filters.country === business.country;
         const matchesCity = !filters.city || filters.city === business.city;
         const matchesRating = !filters.rating || (business.score >= filters.rating && business.score < filters.rating + 1);
